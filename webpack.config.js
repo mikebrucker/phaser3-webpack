6866ef35a6c8487e981dee4c9974bbabbd64e076
Mike Brucker <disposition11@gmail.com>
2018-11-10 11:43:56 -0500

    copy webpack and webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
 	entry: {
@@ -6,8 +8,20 @@ module.exports = {
 		'production-dependencies': ['phaser']
 	},
 	devServer: {
-		contentBase: path.resolve(__dirname, 'dist')
+		contentBase: path.resolve(__dirname, 'dist/')
 	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{
+				from: path.resolve(__dirname, 'index.html'),
+				to: path.resolve(__dirname, 'dist/')
+			}
+		]),
+		new webpack.DefinePlugin({
+			'typeof CANVAS_RENDERER': JSON.stringify(true),
+			'typeof WEBGL_RENDERER': JSON.stringify(true)
+		}),	  
+	],
 	module: {
 		rules: [
 			{
